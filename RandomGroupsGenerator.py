ea266c71d29ef7d74a8f5b18300f96f3558d7cc0
AkiMosi <48480552+AkiMosi@users.noreply.github.com>
2019-07-30 17:11:10 +0530

    Update RandomGroupsGenerator.py

--- RandomGroupsGenerator.py
+++ RandomGroupsGenerator.py
@@ -5,16 +5,22 @@ def genGroup():
     number_of_groups=5
     #number_of_groups=int(input("Enter the Number of groups : "))
     groups=[]
-    names=["Aakash Babu","Ajay Damodar","Akileshvar A","Arun Kumar T","Harini S",
-           "Janani","Jayaram","Joel Anthony Xavier","Kamalam","Kausik R Ipour",
-           "Kavinila","Kaviya","Keerthana","Krishmitha","Mahaboob Basha",
-           "Manoji Kumar","Meera","Midun Raju C","Monisha","Mukund E","Nandhini C",
-           "Neha S","Nitha","Oviya","Prakash B","Rashmi K","Sanjana","Shrinidhi",
-           "Shruthi R","Sreenila","Srinitha","Sruthi B","Sruthi S","Surya N",
-           "Vigneshwaran S","Tarun Kumar M M","Vikhram R A","Visalatchi"]
+    names=["Aakash Babu","Ajay Damodar","Akileshvar A","Arun Kumar T",
+           "Harini S","Janani","Jayaram","Joel Anthony Xavier","Kamalam",
+           "Kausik R Ipour","Kavinila","Kaviya","Keerthana","Krishmitha",
+           "Mahaboob Basha","Manoji Kumar","Meera","Midun Raju C","Monisha",
+           "Mukund E","Nandhini C","Neha S","Nitha","Oviya","Prakash B",
+           "Rashmi K","Sanjana","Shrinidhi","Shruthi R","Sreenila","Srinitha",
+           "Sruthi B","Sruthi S","Surya N","Vigneshwaran S","Tarun Kumar M M",
+           "Vikhram R A","Visalatchi"]
     total_strength=38
-    print("There will be a group with ", total_strength%number_of_groups,'members')
-    
+#    print("There will be a group with ", total_strength%number_of_groups,
+#          'members\n Or there should be a equal spread ?(y/n) : ')
+#    choice=input()
+#    if(choice=='n'):
+#        print("")
+#    else:
+#        print("")
     lst=list(np.arange(1,41,1))
     lst.remove(2)
     lst.remove(15)
@@ -28,6 +34,8 @@ def genGroup():
             lst_copy.remove(temp[j])
             temp[j]=ds[temp[j]]
         groups.append(temp)
+        members_per_group=int(len(lst_copy)/(number_of_groups-i+1))
+        print(len(lst_copy)," ",members_per_group)
     if(len(lst_copy)>0):
         temp=[]
         for i in range(len(lst_copy)):
@@ -48,7 +56,8 @@ def main():
     flag=0
     print("Hello, World")
     while(True):
-        print("\n\n\nEnter the choice : \n1. Generate Groups \n2. Print the Generated Group\n3. Exit")
+        print("\n\n\nEnter the choice : \n1. Generate Groups \n2. Print the Ge"
+              "nerated Group\n3. Exit")
         choice=int(input())
         if(choice==1):
             groups=genGroup()
